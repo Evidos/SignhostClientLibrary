7e2162c8d7febdde73b2eaa695c4ccef3892a7c3
Ron de Groot <ron@nandro.nl>
2015-07-03 16:58:57 +0200

    First commit

---
+++ SignhostClientLibrary/Models/File.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SignhostApiClientLibrary.Models
+{
+    public class File
+    {
+        public Guid Id { get; internal set; }
+        public string Name { get; set; }
+    }
+}
