8078295904eecb3e502a0144a15f540ab9e52b20
Ron de Groot <ron@nandro.nl>
2015-09-18 12:41:06 +0200

    Changed AssemblyInfo.cs

--- SignhostClientLibrary/Properties/AssemblyInfo.cs
+++ SignhostClientLibrary/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SignhostClientLibrary")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A .NET library for the signhost.com api.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SignhostClientLibrary")]
+[assembly: AssemblyCompany("Signhost")]
+[assembly: AssemblyProduct("Signhost")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
