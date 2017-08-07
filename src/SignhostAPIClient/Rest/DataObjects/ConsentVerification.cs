f8653dbec25ec8d27eee88b62f014959a4540be9
Jeroen Asselman <jeroen@asselman.com>
2017-08-07 11:18:35 +0200

    Add Consent verification

---
+++ src/SignhostAPIClient/Rest/DataObjects/ConsentVerification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Signhost.APIClient.Rest.DataObjects
+{
+	/// <summary>
+	/// Adds a consent verification screen
+	/// </summary>
+	public class ConsentVerification
+		: IVerification
+	{
+		/// <summary>
+		/// Gets the <see cref="IVerification.Type"/>.
+		/// </summary>
+		public string Type { get; } = "Consent";
+	}
+}
