559f485d97638488a89f33a548b14b0bb8066a8a
ATimmeh33 <a.timmers@evidos.nl>
2018-10-19 14:13:17 +0200

    Add eIDAS Login verification

---
+++ src/SignhostAPIClient/Rest/JsonConverters/LevelEnumConverter.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Reflection;
+using Newtonsoft.Json;
+using Signhost.APIClient.Rest.DataObjects;
+
+namespace Signhost.APIClient.Rest.JsonConverters
+{
+	/// <summary>
+	/// JSON converter for converting the <see cref="Level"/> enum.
+	/// Invalid values are mapped to <see cref="Level.Unknown"/>.
+	/// </summary>
+	internal class LevelEnumConverter
+		: JsonConverter
+	{
+		/// <inheritdoc/>
+		public override bool CanWrite => false;
+
+		/// <inheritdoc/>
+		public override bool CanConvert(Type objectType)
+			=> IsLevelEnum(GetUnderlyingType(objectType));
+
+		/// <inheritdoc/>
+		public override object ReadJson(
+			JsonReader reader,
+			Type objectType,
+			object existingValue,
+			JsonSerializer serializer)
+		{
+			var value = reader.Value as string;
+
+			if (value != null) {
+				if (Enum.TryParse(value, out Level level)) {
+					return level;
+				}
+
+				return Level.Unknown;
+			}
+
+			return null;
+		}
+
+		/// <inheritdoc/>
+		public override void WriteJson(
+				JsonWriter writer,
+				object value,
+				JsonSerializer serializer)
+			=> throw new NotImplementedException();
+
+		private static Type GetUnderlyingType(Type type)
+			=> type.GetTypeInfo().IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>)
+				? Nullable.GetUnderlyingType(type)
+				: type;
+
+		private static bool IsLevelEnum(Type type)
+			=> type.GetTypeInfo().IsEnum && type == typeof(Level);
+	}
+}
