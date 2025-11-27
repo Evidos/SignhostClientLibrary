cd54588c23ce533a5651f6983a00437c9a28dd82
Anthony Timmers <a.timmers@evidos.nl>
2025-11-27 21:30:23 +0100

    Add converter for Field value

--- src/SignhostAPIClient/Rest/DataObjects/FileMetaData/Field.cs
+++ src/SignhostAPIClient/Rest/DataObjects/FileMetaData/Field.cs
@@ -1,11 +1,17 @@
-﻿namespace Signhost.APIClient.Rest.DataObjects;
+﻿using System.Text.Json.Serialization;
+using Signhost.APIClient.Rest.JsonConverters;
+
+namespace Signhost.APIClient.Rest.DataObjects;
 
 public class Field
 {
 	public FileFieldType Type { get; set; }
 
-	// TO-DO: Can be boolean, number, string, should be fixed in v5.
-	public string? Value { get; set; }
+	/// <summary>
+	/// The value content for the field. Can be a string, number, or boolean.
+	/// </summary>
+	[JsonConverter(typeof(JsonObjectConverter))]
+	public object? Value { get; set; }
 
 	public Location Location { get; set; } = default!;
 }
